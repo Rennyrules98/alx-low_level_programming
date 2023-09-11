ed155a42374167535b9ad4fc43ca8928da689293
Rennyrules98 <rennyrichard15@gmail.com>
2023-09-11 09:48:57 -0700

    3 Alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Return: Always 0 (Success)
+ * Return Always 0 (Success)
  */
 
-int main()
+int main(void)
 {
 	char c;
+
 	char d;
 
 	c = 'a';
@@ -17,8 +18,8 @@ int main()
 			c++;
 		}
 	while
-		(d <= 'z') {
-			putchar(d);
+		(d <= 'Z') {
+			putchar (d);
 			d++;
 		}
 	putchar('\n');
