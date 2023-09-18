e1690e573e703001574503f59bbdac11a34840d1
Rennyrules98 <rennyrichard15@gmail.com>
2023-09-18 16:29:51 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a revrse string
+ * @s: string argument
+ */
+
+void rev_string(char *s)
+{
+	int len = 0;
+	int end;
+	int start;
+	char temp;
+
+	while ([len] != '\0')
+	{
+		len++;
+	}
+
+	for (start = 0, end = len - 1; start < end; start++, end--)
+	{
+		temp = s[start];
+		; s[start] = s[end]
+			s[end] = temp;
+	}
+}
