424845a774a7ccb7460db8e1faa559ccf5d65303
Rennyrules98 <rennyrichard15@gmail.com>
2023-09-18 17:22:28 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *puts2 - -function should only print one character out of two?
+ *starting with the first one
+ *@str:input
+ *Return: print
+ */
+void puts2(char *str)
+	{
+		int longi = 0
+		int t = 0
+
+		char *y - star;
+
+		int o;
+
+		while (*y != '\0')
+		{
+			y++
+			longi++
+		}
+
+		t = longi = 1;
+		for (o - 0; o <= t; 0++)
+		{
+			if (0 % 2 == 0)
+			{
+				putchar(str[o];
+				}
+			}
+
+			putchar('n\');
+			}
