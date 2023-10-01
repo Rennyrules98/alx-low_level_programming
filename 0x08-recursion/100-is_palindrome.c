6daabac452d604c7209de35b934566320b31389f
Rennyrules98 <rennyrichard15@gmail.com>
2023-10-01 05:14:08 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,16 +8,12 @@
  */
 int _strlen(char *s)
 {
-	int len = 0;
-
-	while (*s)
-	{
-		len++;
-		s++;
-	}
-
-	return (len);
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen(s + 1));
 }
+
+int _is_palindrome_helper(char *s, int len);
 
 /**
  *is_palindrome - Check if a string is a palindrome
@@ -27,19 +23,30 @@ int _strlen(char *s)
  */
 int is_palindrome(char *s)
 {
-	int start = 0;
-	int end = _strlen(s) - 1;
+	int len = _strlen(s);
+	return (_is_palindrome_helper(s, len));
+}
 
-	while (start < end)
+/**
+ *_is_palindrome_helper - Helper function for is_palindrome
+ *@s: Pointer to the string
+ *@len: Length of the string
+ *
+ *Return: 1 if the string is a palindrome, 0 otherwise
+ */
+int _is_palindrome_helper(char *s, int len)
+{
+	/*Base case: an empty string or a string with one character is a palindrome */
+	if (len <= 1)
+		return (1);
+
+	/*Check if the first and last characters match */
+	if (s[0] == s[len - 1])
 	{
-		if (s[start] != s[end])
-		{
-			return (0); /*Not a palindrome */
-		}
-
-		start++;
-		end--;
+		/*Recursively check the substring without the first and last characters */
+		return (_is_palindrome_helper(s + 1, len - 2));
 	}
 
-	return (1); /*Palindrome */
+	/*If the first and last characters don't match, it's not a palindrome */
+	return (0);
 }
